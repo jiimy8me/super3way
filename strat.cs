042c2160207b008b5bc08b57c0e9f3bd70d41581
Shaggy <47267279+shxggy3386@users.noreply.github.com>
2023-04-24 09:06:21 -0500

    Update strat.cs

--- strat.cs
+++ strat.cs
@@ -1,16 +1,17 @@
 //@version=5
 strategy("Super 3-Way", overlay=true)
 
+
 // superTrend1
 Periods1 = input(title='ATR Period 1', defval=12)
-src1 = input(ohlc4, title='Source 1')
+sT1src = input(ohlc4, title='Source 1')
 Multiplier1 = input.float(title='ATR Multiplier 1', step=0.1, defval=3.0)
 changeATR1 = input(title='On = ta.atr(Preiods1) Off = ta.sma(ta.tr, Periods1)', defval=true)
 atr1 = changeATR1 ? ta.atr(Periods1) : ta.sma(ta.tr, Periods1)
-up1 = src1 - (Multiplier1 * atr1)
+up1 = sT1src - (Multiplier1 * atr1)
 up11 = nz(up1[1], up1)
 up1 := close[1] > up11 ? math.max(up1, up11) : up1
-dn1 = src1 + (Multiplier1 * atr1)
+dn1 = sT1src + (Multiplier1 * atr1)
 dn11 = nz(dn1[1], dn1)
 dn1 := close[1] < dn11 ? math.min(dn1, dn11) : dn1
 trend1 = 1
@@ -30,14 +31,14 @@ fill(mPlot1, dnPlot1, title='Down Trend 1 Highligter', color=color.new(shortFill
 
 // superTrend2
 Periods2 = input(title='ATR Period 2', defval=11)
-src2 = input(ohlc4, title='Source 2')
+sT2src = input(ohlc4, title='Source 2')
 Multiplier2 = input.float(title='ATR Multiplier 2', step=0.1, defval=2.0)
 changeATR2 = input(title='On = ta.atr(Preiods2) Off = ta.sma(ta.tr, Periods2)', defval=true)
 atr2 = changeATR2 ? ta.atr(Periods2) : ta.sma(ta.tr, Periods2)
-up2 = src2 - (Multiplier2 * atr2)
+up2 = sT2src - (Multiplier2 * atr2)
 up12 = nz(up2[1], up2)
 up2 := close[1] > up12 ? math.max(up2, up12) : up2
-dn2 = src2 + (Multiplier2 * atr2)
+dn2 = sT2src + (Multiplier2 * atr2)
 dn12 = nz(dn2[1], dn2)
 dn2 := close[1] < dn12 ? math.min(dn2, dn12) : dn2
 trend2 = 1
@@ -57,14 +58,14 @@ fill(mPlot2, dnPlot2, title='Down Trend 2 Highligter', color=color.new(shortFill
 
 // superTrend3
 Periods3 = input(title='ATR Period 3', defval=10)
-src3 = input(ohlc4, title='Source 3')
+sT3src = input(ohlc4, title='Source 3')
 Multiplier3 = input.float(title='ATR Multiplier 3', step=0.1, defval=1.0)
 changeATR3 = input(title='On = ta.atr(Preiods3) Off = ta.sma(ta.tr, Periods3)', defval=true)
 atr3 = changeATR3 ? ta.atr(Periods3) : ta.sma(ta.tr, Periods3)
-up3 = src3 - (Multiplier3 * atr3)
+up3 = sT3src - (Multiplier3 * atr3)
 up13 = nz(up3[1], up3)
 up3 := close[1] > up13 ? math.max(up3, up13) : up3
-dn3 = src3 + (Multiplier3 * atr3)
+dn3 = sT3src + (Multiplier3 * atr3)
 dn13 = nz(dn3[1], dn3)
 dn3 := close[1] < dn13 ? math.min(dn3, dn13) : dn3
 trend3 = 1
@@ -82,42 +83,77 @@ shortFillColor3 = color.purple
 fill(mPlot3, upPlot3, title='Up Trend 3 Highligter', color=color.new(longFillColor3, 90))
 fill(mPlot3, dnPlot3, title='Down Trend 3 Highligter', color=color.new(shortFillColor3, 90))
 
-// uptrend and dntrend
+// upTrend and dnTrend
 upTrend = (trend3 == 1) and (trend2 == 1) and (trend1 == 1)
 dnTrend = (trend3 == -1) and (trend2 == -1) and (trend1 == -1)
 
-// broken
+// trend broken
 brTup = ((trend3 == -1) and (trend2 == -1)) or ((trend3 == -1) and (trend1 == -1)) or ((trend2 == -1) and (trend1 == -1))
 brTdn = ((trend3 == 1) and (trend2 == 1)) or ((trend3 == 1) and (trend1 == 1)) or ((trend2 == 1) and (trend1 == 1))
 
-// only long if price closes above the 3 supertrends
+// enter long if price closes above 3 superTrends
 longRule = upTrend == true
-// only short if price closes below the 3 supertrends
+// enter short if price closes below 3 superTrends
 shortRule = dnTrend == true
 
-// close long if 2 of 3 Supertrends are down
+// close long if 2 of 3 superTrends are down
 longExit = brTup == true
-// close short if 2 of 3 Supertrends are up
+// close short if 2 of 3 superTrends are up
 shortExit = brTdn == true
 
 // strategy rules
+pctComm = 0.0006
+aEntry = strategy.position_avg_price
+pSize = strategy.position_size
+oProfit = strategy.openprofit
+pComm = (pctComm * aEntry)
+
 if longRule
     strategy.entry("Long", strategy.long)
-    if strategy.position_size < 0
-        aEntry = strategy.position_avg_price
-        lEntry = (aEntry + (aEntry * 0.0007))
-        longStop = (lEntry - (lEntry * 0.05))
-        strategy.exit("Long Stop", "Long", loss=longStop)
+    if strategy.position_size > 0
+        lEntry = (aEntry + pComm)
+        pValue = ((aEntry * pSize) - ((aEntry * pSize) * pctComm))
+        pProfit = (oProfit / pValue)
+        longStop10p = lEntry * 1.075
+        longStop7p5 = lEntry * 1.05
+        longStop5p = lEntry * 1.025
+        longStop2p5 = lEntry
+        longStop = lEntry * 0.95
+        if pProfit >= 0.1
+            strategy.exit("Long", "Stop Loss", stop=longStop10p)
+        else if pProfit >= 0.075
+            strategy.exit("Long", "Stop Loss", stop=longStop7p5)
+        else if pProfit >= 0.05
+            strategy.exit("Long", "Stop Loss", stop=longStop5p)
+        else if pProfit >= 0.025
+            strategy.exit("Long", "Stop Loss", stop=longStop2p5)
+        else
+            strategy.exit("Long", "Stop Loss", stop=longStop)
 
 if shortRule
     strategy.entry("Short", strategy.short)
-    if strategy.position_size > 0
-        aEntry = strategy.position_avg_price
-        sEntry = (aEntry - (aEntry * 0.0007))
-        shortStop = (sEntry + (sEntry * 0.05))
-        strategy.exit("Short Stop", "Short", loss=shortStop)
+    if strategy.position_size < 0
+        sEntry = (aEntry - pComm)
+        sValue = ((aEntry * pSize) + ((aEntry * pSize) * pctComm))
+        sProfit = (oProfit / sValue)
+        shortStop10p = sEntry * 0.925
+        shortStop7p5 = sEntry * 0.95
+        shortStop5p = sEntry * 0.975
+        shortStop2p5 = sEntry
+        shortStop = sEntry * 1.05
+        if sProfit >= 0.1
+            strategy.exit("Short", "Stop Loss", stop=shortStop10p)
+        else if sProfit >= 0.075
+            strategy.exit("Short", "Stop Loss", stop=shortStop7p5)
+        else if sProfit >= 0.05
+            strategy.exit("Short", "Stop Loss", stop=shortStop5p)
+        else if sProfit >= 0.025
+            strategy.exit("Short", "Stop Loss", stop=shortStop2p5)
+        else
+            strategy.exit("Short", "Stop Loss", stop=shortStop)
 
 if longExit
     strategy.close("Long", "Long Exit")
+
 if shortExit
     strategy.close("Short", "Short Exit")
